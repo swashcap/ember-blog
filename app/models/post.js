b02c8f17c1c1a2ef89bd0e2d42f9e1026a931423
Cory Reed <swashcap@gmail.com>
2015-03-22 20:10:37 -0700

    Add blog post date feature.

--- app/models/post.js
+++ app/models/post.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   author: DS.belongsTo('author'),
   title: DS.attr('string'),
-  body: DS.attr('string')
+  body: DS.attr('string'),
+  date: DS.attr('date')
 });
--- server/mocks/posts.js
+++ server/mocks/posts.js
@@ -3,13 +3,15 @@ var posts = [
     id: 1,
     title: 'Bananas',
     author: 1,
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt eros elementum nibh iaculis laoreet sit amet ut mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consequat felis non ipsum malesuada luctus. Morbi non tellus placerat, dignissim lorem eget, molestie ipsum. Pellentesque molestie et nisi eget accumsan. In sapien ligula, vehicula vel odio nec, tempor cursus libero.'
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt eros elementum nibh iaculis laoreet sit amet ut mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consequat felis non ipsum malesuada luctus. Morbi non tellus placerat, dignissim lorem eget, molestie ipsum. Pellentesque molestie et nisi eget accumsan. In sapien ligula, vehicula vel odio nec, tempor cursus libero.',
+    date: new Date(2015, 1, 2)
   },
   {
     id: 2,
     title: 'Apples',
     author: 1,
-    body: 'Donec consectetur purus elit, ut imperdiet neque ultrices quis. Morbi ligula lorem, placerat et varius sed, accumsan nec dui. Maecenas commodo porta ultrices. Integer felis enim, iaculis at ipsum in, mattis hendrerit mauris. Aliquam sollicitudin feugiat facilisis. Duis ut orci luctus, hendrerit nisl eget, ullamcorper urna. Etiam consectetur lobortis ipsum sit amet rhoncus. Vivamus tempus a lorem nec dictum. Nunc placerat augue quis ante egestas semper. Cras in ex quis dolor feugiat scelerisque vitae vel libero.'
+    body: 'Donec consectetur purus elit, ut imperdiet neque ultrices quis. Morbi ligula lorem, placerat et varius sed, accumsan nec dui. Maecenas commodo porta ultrices. Integer felis enim, iaculis at ipsum in, mattis hendrerit mauris. Aliquam sollicitudin feugiat facilisis. Duis ut orci luctus, hendrerit nisl eget, ullamcorper urna. Etiam consectetur lobortis ipsum sit amet rhoncus. Vivamus tempus a lorem nec dictum. Nunc placerat augue quis ante egestas semper. Cras in ex quis dolor feugiat scelerisque vitae vel libero.',
+    date: new Date(2015, 2, 3)
   }
 ];
 var authors = [{
