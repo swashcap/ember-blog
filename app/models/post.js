335d17645209cf16a2d34fd85ce0bcf82a3eae70
Cory Reed <swashcap@gmail.com>
2015-03-22 19:30:22 -0700

    Add stuff for single post.

--- app/models/post.js
+++ app/models/post.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   author: DS.belongsTo('author'),
-  title: DS.attr('string')
+  title: DS.attr('string'),
+  body: DS.attr('string')
 });
